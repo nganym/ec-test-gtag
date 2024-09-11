c069cbf5f8256d36a2c76cdae7b251f90c683273
nganym <nym.test.anna@gmail.com>
2024-09-11 10:47:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -66,6 +66,35 @@ document
     message.innerText = "Successfully submit hashed form";
   });
 
+document.querySelector("#undefined-form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  const form = event.target;
+
+
+
+
+  gtag("event", "conversion", {
+    allow_custom_scripts: true,
+    send_to: "DC-11107970/conve0/ectes0+standard",
+    user_data: {
+      email: undefined,
+      phone_number: undefined,
+    },
+  });
+
+  // window.dataLayer = window.dataLayer || [];
+  // window.dataLayer.push({
+  //   event: "annaPurchaseEC",
+  //   customerEmail: formObject.email,
+  //   customerPhone: formObject.phone,
+  // });
+
+  console.log("Unhashed Customer Email Sent", formObject.email);
+  console.log("Unhashed Customer Phone Sent", formObject.phone);
+
+  message.innerText = "Successfully submit undefined form";
+});
+
 async function hash(string) {
   const utf8 = new TextEncoder().encode(string);
   const hashBuffer = await crypto.subtle.digest("SHA-256", utf8);
